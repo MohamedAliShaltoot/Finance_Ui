95e78a7b290b37335e1d4113035f5cd8441bca79
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-02-11 21:33:37 +0200

    app_colors,fonts,style,constants and themeData

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor =  Color(0xFF617AFD);
+  static const Color secondaryColor =  Color(0xFF8391A1);
+  static const Color blackColor =  Colors.black;
+  static const Color grayColor =  Color(0xff9CA4AB);
+  static const Color whiteColor =  Colors.white;
+}
---
+++ lib/core/constants.dart
@@ -0,0 +1,23 @@
+class Constants {
+  static const String appName = 'Finance UI';
+  static const String loginWelcomeTitle = 'Welcome back! Again!';
+
+  static const String loginButton = 'Login';
+  static const String registerButton = 'Register';
+  static const String forgotPasswordTitle = 'Forgot Password?';
+  static const String forgotPasswordSubTitle =
+      " Don't worry! It occurs. Please enter the email address linked with your account.";
+
+  static const String registerWelcomeTitle = 'Hello! Register to get started';
+  static const String oTPVerificationTitle = 'OTP Verification';
+  static const String oTPVerificationSubTitle = 'Enter the verification code we just sent on your email address.';
+
+    static const String createNewPasswordTitle = 'Create new password';
+    static const String createNewPasswordSubTitle = 'Your new password must be unique from those previously used.';
+
+
+
+
+
+}
+//
