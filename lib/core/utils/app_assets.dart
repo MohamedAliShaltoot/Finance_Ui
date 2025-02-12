62682575dd3ebd3b2cfca56492aa9a5b175fbee2
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-02-12 21:41:02 +0200

    add images paths

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,13 @@
+class AppAssets {
+  
+  static const String onBoardImage1 = "assets/images/onBoardingImage.png";
+  static const String appleSvg = "assets/images/apple.svg";
+  static const String googleSvg = "assets/images/google.svg";
+  static const String facebookSvg = "assets/images/facebook.svg";
+  static const String changedIconSvg = "assets/images/Sticker.png";
+
+
+
+
+
+}
